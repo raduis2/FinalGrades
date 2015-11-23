620ebf1c240f331fe5a49c8d6f9885e54d4a214a
Radu Siminiceanu <radu@buddy.com>
2015-11-23 13:30:35 -0800

    Initial solutions.

---
+++ Grades8.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Grades {
+
+  public static class Record {
+    public String studentId;
+    public String testId;
+    public Double score;
+
+    public Record(final String student, final String test, final Double score) {
+      this.studentId = student;
+      this.testId = test;
+      this.score = score;
+    }
+
+    public String toString() {
+      return new String(studentId + "@" + testId + ":" + score);
+    }
+  }
+
+  public static Double getTopK(Map<String, Double> scores, int K) {
+    DoubleSummaryStatistics stats = scores.stream().collect(
+         Collectors.summarizingDouble(e -> e.getValue()));
+    return stats.getAverage();
+  }
+
+  public static Collection<Record> computeTopK(int K, Collection<Record> testScores) {
+    List<Record> finalScores = new ArrayList<Record>();
+    Map<String, Map<String, Double>> map = new HashMap<String, Map<String, Double>>();
+    for (Record record : testScores) {
+      if (map.containsKey(record.studentId)) {
+        Map<String,Double> tests = map.get(record.studentId);
+        if (tests.containsKey(record.testId)) {
+          if (record.score.doubleValue() > tests.get(record.testId).doubleValue()) {
+            tests.put(record.testId, record.score);
+          }
+        }
+        else {
+          tests.put(record.testId, record.score);
+        }
+      } else {
+        Map<String, Double> tests = new HashMap<String, Double>();
+        tests.put(record.testId, record.score);
+        map.put(record.studentId, tests);
+      }
+    }
+    for (Map.Entry<String, Map<String, Double>> tests : map.entrySet()) {
+      finalScores.add(new Record(tests.getKey(), "final", getTopK(tests.getValue(), K)));
+    }
+    return finalScores;
+  }
+
+  private static void init() {
+    testScores = new ArrayList<Record>();
+    testScores.add(new Record("Adam", "Math", 10.0));
+    testScores.add(new Record("Adam", "English", 10.0));
+    testScores.add(new Record("Adam", "English", 8.0));
+    testScores.add(new Record("Adam", "French", 9.0));
+    testScores.add(new Record("Adam", "History", 7.0));
+    testScores.add(new Record("Adam", "Biology", 5.0));
+    testScores.add(new Record("Adam", "Music", 8.0));
+    testScores.add(new Record("Beth", "Math", 8.5));
+    testScores.add(new Record("Beth", "English", 10.0));
+    testScores.add(new Record("Beth", "French", 8.0));
+    testScores.add(new Record("Beth", "French", 9.0));
+    testScores.add(new Record("Beth", "History", 8.0));
+    testScores.add(new Record("Beth", "Music", 10.0));
+    testScores.add(new Record("Beth", "Music", 10.0));
+    testScores.add(new Record("Beth", "Music", 3.0));
+  }
+
+  public static void main(String[] args) {
+    Collection testScores;
+    init();
+    System.out.println(testScores.toString());
+    Collection<Record> finalScores = computeTopK(Integer.parseInt(args[0]), testScores);
+    System.out.println(finalScores.toString());
+  }
+}
